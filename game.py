074a8cc03dd99b5e6b12260f8f053a9c185288e3
JiaRong83 <125329875+JiaRong83@users.noreply.github.com>
2023-08-03 08:09:23 +0000

    import random

--- game.py
+++ game.py
@@ -1,3 +1,8 @@
+import random
+
+
 def pull(n: int) -> str:
     """Takes an integer n and rolls that number of times, returning a string name of the item/character received.""" 
+    pass
+def fear(n: int) -> str:
     pass
