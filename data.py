89b24210ad4befae68bd52af965357dc083c4c69
TheLegendaryZHR <124885039+TheLegendaryZHR@users.noreply.github.com>
2023-08-03 16:21:12 +0800

    Create data.py

---
+++ data.py
@@ -0,0 +1,15 @@
+import json
+
+
+class Item:
+    def __init__(self, ...):
+        ...
+
+
+class Creature:
+    def __init__(self, ...):
+        ...
+
+
+with open("content/items.json", "r") as f:
+    items = json.load(f)
