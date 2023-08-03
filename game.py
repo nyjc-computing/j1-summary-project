569ba887ece585ab27671e9b0ddf3545ad3fca71
56LogicErrorsAnd109SyntaxErrors <125201147+56LogicErrorsAnd109SyntaxErrors@users.noreply.github.com>
2023-08-03 08:34:03 +0000

    edited game.py

---
+++ game.py
@@ -0,0 +1,13 @@
+class MUDGame:
+    def __int__(self):
+        pass
+
+    def prompt(self):
+        pass
+        
+    def run(self):
+        pass
+
+class roomGeneration:
+    def __init__(self):
+        pass
