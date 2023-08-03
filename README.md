709256a20839337933967737c80793d6b19958f0
wciltea <125111866+wciltea@users.noreply.github.com>
2023-08-03 07:49:03 +0000

    valorant is a terrible game

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Members
 
 - Member 1
-- Member 2
+- Vincent
 - Member 3
 - Member 4
 
--- main.py
+++ main.py
@@ -2,3 +2,7 @@
 
 if __name__ == "__main__":
     pass
+
+def why(valorant):
+    if valorant == valorant:
+        print('valorant is cringe')
