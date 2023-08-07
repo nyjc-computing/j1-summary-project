ed62eebae1f78e373228bc3563377f3066731512
wciltea <125111866+wciltea@users.noreply.github.com>
2023-08-07 08:45:41 +0000

    test skeleton

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 import game
+class CodeError(Exception):
+    pass
+roomlist = []
 
-if i want to count number of creatures i need to know the data structure im taking as an input
+'''def move_check(room) -> str:
+    if room not in roomlist:
+        raise SkillIssue('Player moved to a room that does not exist')
 
-if i want to move the player somewhere they cant reach in one turn i need to know if we're giving them a multiple choice UI or an open-ended prompt
+move_check('piss')'''
+
+def boss_present():
+    if boss not in maze:
+        raise CodeError('You removed the end game method')
+
+def player_present():
+    if player not in maze:
+        raise CodeError('How did you even make this happen')
