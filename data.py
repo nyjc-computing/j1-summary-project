889b8808e9a846f334bf4f71127f7ca886c9483c
saathvik28 <125167997+saathvik28@users.noreply.github.com>
2023-08-07 07:43:29 +0000

    New Room and Grid classes. Pls Approve

---
+++ data.py
@@ -0,0 +1,13 @@
+class room:
+    def __init__(self, name : str, paths : list, creature : bool, orb : bool):
+        self.name = name
+        self.paths = paths
+        self.creature = creature
+        self.orb = orb
+
+class grid:
+    def __init__(rooms):
+        grid.start = None
+        grid.end = None
+    def arrange(rooms):
+        pass
