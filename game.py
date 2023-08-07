cc5f36700528253553ef639cdb95209453a999e7
xinyu2309 <125541832+xinyu2309@users.noreply.github.com>
2023-08-07 08:55:22 +0000

    wrote the gameplay loop

--- game.py
+++ game.py
@@ -1,11 +1,59 @@
 #File containing the code for the game
 import data
+import random
+
+
 
 class MUDGame:
     def __init__(self):
         self.gameover = False # default
         self.won = False # default
-        
+        maze = Labyrinth()
+        maze.generate()
+        steve = Steve()
+
+    
+    def introduce(self): 
+        """
+        Starting interface of the game
+        """
+        username = input('Enter your username:')
+        print(f'{username}, OH NO YOU ARE TRAPPED! \nYou will go through a series of rooms that may give you items or have ANGRY creatures wanting you DEAD :P \nKill them all, especially the boss to escape! \nGOOD LUCK ;D')
+
+
+
+
+    
+    def encounter(creature):
+        """
+        Options when encountering creatures
+        """
+        option = None
+        while option == None:
+            # loop to prompt valid input
+            print(f'You have encountered {creature}! \nDo you want to attack or escape?')
+            print('1. Attack \n2. Escape')
+            option = input('Your choice:')
+            if option == 1:
+                # player choose to attack creature
+                pass
+            elif option == 2:
+                # player choose to escape
+                chance = random.randint(1, 2)
+                if chance == 1:
+                    # player escapes
+                    pass
+                else:
+                    # player failed to escape
+                    # player receive damage
+                    pass
+            else:
+                option = None
+
+    
+                    
+                
+            
     
     def run(self):
         """
@@ -16,6 +64,20 @@ class MUDGame:
 - losescreen + lose conditions
 - 
         """
-        maze = Labyrinth()
-        start = maze.generate()
-        
+        self.introduce()
+        while not self.gameover():
+            self.show_status()
+            # show status
+            self.show_options()
+            # show player action options
+            choice = self.prompt_player()
+            # prompt player to take actions
+            self.validate(choice)
+            # validate choice
+            self.do_action()
+            self.update()
+            # update
+            
+            
+
+
