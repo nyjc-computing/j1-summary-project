7d5c7b61fccadaa66d48245c9e389222dc51b055
wciltea <125111866+wciltea@users.noreply.github.com>
2023-08-11 06:15:06 +0000

    i need more info please thank you

--- test.py
+++ test.py
@@ -3,11 +3,6 @@ class CodeError(Exception):
     pass
 roomlist = []
 
-'''def move_check(room) -> str:
-    if room not in roomlist:
-        raise SkillIssue('Player moved to a room that does not exist')
-
-move_check('piss')'''
 
 def boss_present():
     if boss not in maze:
@@ -16,3 +11,5 @@ def boss_present():
 def player_present():
     if player not in maze:
         raise CodeError('How did you even make this happen')
+
+how write test without data structure
