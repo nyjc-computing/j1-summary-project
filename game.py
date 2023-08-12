05abadd6ad81248e67928acadde324ada8a97f40
56LogicErrorsAnd109SyntaxErrors <125201147+56LogicErrorsAnd109SyntaxErrors@users.noreply.github.com>
2023-08-12 02:58:34 +0000

    .

--- game.py
+++ game.py
@@ -5,6 +5,18 @@ def accuracy_calc(accuracy : int) -> bool:
     temp = [True] * accuracy + [False] *(100 - accuracy)
     return temp[random.randint(0, len(temp) - 100)]
 
+def defeat(players : list) -> bool:
+    for player in players:
+        if not player.is_defeated():
+            return False
+    return True
+
+def victory(enemies : list) -> bool:
+    for enemy in enemies:
+        if not enemy.is_defeated():
+            return False
+    return True
+    
 class MUDGame:
     def __init__(self):
         # self.spawn = Room('home', up='closed')
@@ -25,9 +37,27 @@ class MUDGame:
             self.player1 = Freddy()
         while not self.gameOver:
             while not self.current_room.is_encounter():
-                #moving in current room
+                #prompt movement
                 self.current_room.display()
                 input = self.current_room.grid.prompt_movement()
+                #entering next room
+                if self.current_room.grid.get_position() == [0, 2] and input == 'w':
+                    self.current_room.nextRoom(input)
+                    self.current_room.grid.move([4, 2])
+                    continue
+                elif self.current_room.grid.get_position() == [2, 0] and input == 'a':
+                    self.current_room.nextRoom(input)
+                    self.current_room.grid.move([2, 4])
+                    continue
+                elif self.current_room.grid.get_position() == [4, 2] and input == 's':
+                    self.current_room.nextRoom(input)
+                    self.current_room.grid.move([0, 2])
+                    continue
+                elif self.current_room.grid.get_position() == [2, 4] and input == 'd':
+                    self.current_room.nextRoom(input)
+                    self.current_room.grid.move([2, 0])
+                    continue
+                #moving in current room
                 while input.lower() not in 'wasd':
                     input = self.current_room.prompt_movement()
                 if input.lower() == 'w':
@@ -50,19 +80,24 @@ class MUDGame:
                 if self.current_room.grid.is_item():
                     item = self.current_room.grid.grid[current_position[0]][current_position[1]]['item']
                     data.add_item(item)
-                        
-                    
-                #entering next room
-                if self.current_room.grid.get_position():
-                    self.current_room.nextRoom()
-                    self.current_room.grid.move()
-                    continue
             #Combat Start
-            while self.current_room.is_encounter():
+            if self.current_room.is_encounter():
+                #Determine turn order
+                player_list = [self.player1, self.player2, self.player3, self.player4]
+                enemy_list = self.current_room.grid.get_enemies()
+                turn_order = []
+                i = 0
+                while len(player_list) != 0 and len(enemy_list) != 0:
+                    if player_list[i] != None:
+                        turn_order.append(player_list[i])
+                    else:
+                        player_list.pop(i)
+                    turn_order.append(enemy_list[i])
+                    i += 1
+                #Combat
+                while not defeat(player_list) and not victory(enemy_list):
+                    
                 
-                    
-                    
-
             
             
                 
