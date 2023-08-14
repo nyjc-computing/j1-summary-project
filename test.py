1a2f4fda516078cff67ca5eb18cbcc67133caf09
yxlyx <85774423+yxlyx@users.noreply.github.com>
2023-08-14 01:36:07 +0000

    test

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 #File for QAE
 
+from data import Labyrinth
+
+def test_labyrinth():
+    try:
+        lb = Labyrinth()
+    except:
+        import pdb; pdb.set_trace()
+        
 from game import MUDGame
 
 def test_game():
     # Write code to test the game object here
     # Raise an error if the test fails
 
-
+test_labyrinth()
 test_game()
