8fd57bbce90ab1be9a2ab6e5e3e8337975c6245c
yxlyx <85774423+yxlyx@users.noreply.github.com>
2023-08-14 01:56:35 +0000

    test

--- test.py
+++ test.py
@@ -2,17 +2,35 @@
 
 from data import Labyrinth
 
-def test_labyrinth():
+def test_lbr_init():
     try:
         lb = Labyrinth()
     except:
         import pdb; pdb.set_trace()
+
+lb = Labyrinth()
+
+# def test_lbr_repr():
+#     #not implemented
+#     pass
+
+# def test_lbr_generate():
+#     try:
+#         lb.generate()
+#         print("OK")
+#     except:
+#         import pdb; pdb.set_trace()
         
-from game import MUDGame
+# from game import MUDGame
 
-def test_game():
-    # Write code to test the game object here
-    # Raise an error if the test fails
+# def test_game():
+#     # Write code to test the game object here
+#     # Raise an error if the test fails
+#     pass
 
-test_labyrinth()
-test_game()
+# test_lbr_init()
+# test_lbr_repr()
+# test_lbr_generate()
+# test_game()
+
+lb.generate()
