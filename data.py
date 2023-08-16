4a17cbd6bcb2f0d579e889d79b66359b5903be2c
JiaRong83 <125329875+JiaRong83@users.noreply.github.com>
2023-08-16 16:47:21 +0000

    Patch 1.17: springtrap and glitchtrap added

--- data.py
+++ data.py
@@ -1,30 +1,166 @@
 import random
+import time
 
 #Rooms
 class Room:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.visited = False
-        self.contents = []
-
-    def enter(self):
-        self.visited = True
-        print(f"You have entered ({self.x}, {self.y}).")
-    
-class Grid:
-   def __init__(self, x=2, y=2):
-       self.grid = [{0:None, 1:None, 2:{'type' : 'creature', 'creatures' : []}, 3:{'type' : 'item', 'items':[]}, 4:None}]
-       self.coordinates = [x, y]
-
-   def is_encounter(self):
-       if
+    def __init__(self, x = 2, y = 2, up = None, down = None, left = None, right = None, number = 0):
+        #next rooms
+        self.up = up
+        self.down = down 
+        self.right = right
+        self.left = left
+        self.count = number
+        connections = random.randint(1, 3)
+        next_rooms = [self.up, self.down, self.left, self.right]
+        for room in next_rooms:
+            if room != None:
+                next_rooms.remove(room)
+        while connections != 0:
+            next_room = random.randint(0, 2)
+            next_rooms[next_room] = 'closed'
+            next_rooms.remove(next_room)
+            connections = connections - 1
             
-def start_room() -> 'Room':
-    pass
+        self.grid = Grid(x, y)
 
-def get_position():
-    pass
+    def display_room(self):
+        pass
+
+    def is_next_room(self, next : str) -> bool:
+        if next.lower() == 'w':
+            if self.up == None:
+                return False
+            return True
+        elif next.lower() == 'a':
+            if self.left == None:
+                return False
+            return True
+        elif next.lower() == 's':
+            if self.down == None:
+                return False
+            return True
+        elif next.lower() == 'd':
+            if self.right == None:
+                return False
+            return True
+
+    def next_room(self, next : str) -> None:
+        '''
+        User moves to next room. Depending on the input, move to room above, below, left or right. Also, check if next room for given input exists.
+        '''
+        if next.lower() == 'w':
+            if self.up == None:
+                print('It seems that this door is locked.')
+            else:
+                prev = self
+                self.up = Room(up = prev, number=self.countRoom())
+                self = self.up
+        if next.lower() == 's':
+            if self.down == None:
+                print('It seems that this door is locked.')
+            else:
+                prev = self
+                self.down = Room(down = prev, number=self.countRoom())
+                self = self.down
+        if next.lower() == 'a':
+            if self.left == None:
+                print('It seems that this door is locked.')
+            else:
+                prev = self
+                self.left = Room(right = prev, number=self.countRoom())
+                self = self.left
+        if next.lower() == 'd':
+            if self.right == None:
+                print('It seems that this door is locked.')
+            else:
+                prev = self
+                self.right = Room(left = prev, number=self.countRoom())
+                self = self.right
+    def current_room(self) -> 'Room':
+        '''
+        Returns the current room
+        '''
+        return self
+
+    def countRoom(self):
+        self.count += 1
+        return self.count
+
+
+class Grid:
+    def __init__(self, x, y):
+        self.grid = [{0 : None, 1 : None, 2 : None, 3 : None, 4 : None},
+                    {0 : None, 1 : None, 2 : None, 3 : None, 4 : None},
+                    {0 : None, 1 : None, 2 : None, 3 : None, 4 : None},
+                    {0 : None, 1 : None, 2 : None, 3 : None, 4 : None},
+                    {0 : None, 1 : None, 2 : None, 3 : None, 4 : None}]
+        i = 0
+        #Spawning creatures
+        while i < 5:
+            if self.grid[random.randint(0, 4)][random.randint(0, 4)] == None:
+                self.grid[random.randint(0, 4)][random.randint(0, 4)] = {'type' : 'creature', 'creatures':[]}
+            i = i + 1
+        k = 0
+        #Spawning items
+        while k < 5:
+            if self.grid[random.randint(0, 4)][random.randint(0, 4)] == None:
+                self.grid[random.randint(0, 4)][random.randint(0, 4)] = {'type' : 'items', 'items':[]}
+        self.coordinates = [x, y]
+        k = k + 1
+        
+    def get_position(self) -> list:
+        '''
+        Return user position
+        '''
+        return self.coordinates
+    def prompt_movement(self) -> str:
+        '''
+        Prompt the user for a movement and return the direction to move. Also, to view inventory, user types open inventory
+        '''
+        print("Type 'wasd' to move, open the inventory by typing 'inventory'")
+        action = input('Type an action:')
+        return action
+        
+    def move(self, position : list):
+        '''
+        Update user position and coordinates in the room
+        '''
+        self.coordinates = position
+    
+    def is_encounter(self):
+        '''
+        Return true if user coordinates are currently on a creature tile.
+        '''
+        if self.grid[self.get_position()[0]][self.get_position()[1]]['type'] == 'creature':
+            return True
+        else:
+            return False
+
+    def get_enemies(self):
+        '''
+        Return the enemies on that tile
+        '''
+        return self.grid[self.get_position()[0]][self.get_position()[1]]['creatures']
+    def is_item(self):
+        '''
+        Return true if user coordinates are currently on a item tile.
+        '''
+        if self.grid[self.get_position()[0]][self.get_position()[1]]['type'] == 'item':
+            return True
+        else:
+            return False
+            
+    def get_item(self, coordinates : list) -> str:
+        '''
+        If user is on an item tile, return the item on that tile
+        '''
+        return self.grid[self.get_position()[0]][self.get_position()[1]]['items']
+    def remove(self):
+        '''
+        After a defeating a creature or picking up an item, remove it from the grid
+        '''
+        self.grid[self.get_position()[0]][self.get_position()[1]] = None
+        
 
 
 #Start
@@ -174,7 +310,6 @@ class GB:
         else:
             for status in self.status:
                 print(f'Status: {status}')
-        target.prompt_action(self)
 
     def attack(self, target):
         if 'sleep' in self.status:
@@ -201,6 +336,181 @@ class GB:
                     print('The attack missed!')
         print('---------------------------------------------------------')
 
+
+class Springtrap:
+    def __init__(self, name, evasion=20, status=None, counter=0, health=300, damage=0):
+        self.name = name
+        self.health = health
+        self.evasion = evasion
+        self.counter = counter
+        self.status = status if status is not None else []
+        self.damage = damage
+
+    def take_damage(self, damage: int):
+        self.health -= damage
+        Glitchtrap.spawn()
+
+    def add_status(self, status):
+        self.status.append(status)
+
+    def remove_status(self, status):
+        if status in self.status:
+            self.status.remove(status)
+
+    def has_status(self, status):
+        return status in self.status
+
+    def turn_end(self):
+        self.counter -= 1
+
+    def get_stats(self, target):
+        print(f"{self.name}'s stats")
+        print(f"HP: {self.health}")
+        #print(f"Light level: {self.light}")
+        if self.status == []:
+            print('Status: No statuses.')
+        else:
+            for status in self.status:
+                print(f'Status: {status}')
+
+    def encounter():
+        if Room.current_room() == '?':
+            print('You notice the pungent smell of decaying matter.')
+            time.sleep(2)
+            print('Then, you hear the clanking of metal wires and robotic movement.')
+            time.sleep(2)
+            print('Finally, you see him.')
+            time.sleep(2)
+            print('Springtrap.')
+        
+    def attack(self, target):
+        if 'sleep' in self.status:
+            print(f'{self.name} is asleep!')
+            sleep(self)
+        else:
+            print(f"{self.name} attacks {target.name}!")
+            n = random.randint(1, 3)
+            if n == '1':
+                print(f'{self.name} used Phantom Mirage!')
+                #reduce target's accuracy
+            if n == '2':
+                if accuracy(40) == True:
+                    print(f'{self.name} used Decaying Grasp!')
+                    self.damage += 30
+                    target.take_damage(self.damage)
+                    self.damage -= 30
+                    #reduce target's attack
+                else:
+                    print('The attack missed!')
+            if n == '3':
+                if accuracy(15) == True:
+                    print(f'{self.name} used Eternal Torment!')
+                    self.damage += 85
+                    target.take_damage(self.damage)
+                    self.damage -= 85
+                else:
+                    print('The attack missed!')
+        print('---------------------------------------------------------')
+
+
+
+class Glitchtrap:
+    def __init__(self, name, status=None, counter=0, health=250, damage=0):
+            self.name = name
+            self.health = health
+            self.counter = counter
+            self.status = status if status is not None else []
+            self.damage = damage
+    
+    def take_damage(self, damage: int):
+            self.health -= damage
+            if self.health <= 0:
+                print(f"{self.name} has died!")
+    
+    def add_status(self, status):
+            self.status.append(status)
+    
+    def remove_status(self, status):
+            if status in self.status:
+                self.status.remove(status)
+    
+    def has_status(self, status):
+            return status in self.status
+    
+    def turn_end(self):
+            self.counter -= 1
+    
+    def get_stats(self, target):
+        print(f"{self.name}'s stats")
+        print(f"HP: {self.health}")
+        #print(f"Light level: {self.light}")
+        if self.status == []:
+            print('Status: No statuses.')
+        else:
+            for status in self.status:
+                print(f'Status: {status}')
+
+    def spawn():
+        if Springtrap.health <= 0:
+            print("Springtrap has died!")
+            time.sleep(2)
+            print('Or has he?')
+            time.sleep(2)
+            print('Springtrap: Did you really think this would be enough to finish me?')
+            time.sleep(2)
+            print('Springtrap: Death has never been my concern.')
+            time.sleep(3)
+            print('Springtrap: You forget,')
+            time.sleep(3)
+            print('Springtrap: I always come back.')
+            time.sleep(4)
+            print('You watch as the decaying bunny is encapsulated in digital code, turning him into another bunny with stitches running down his sides as he chuckles.')
+            time.sleep(5)
+            print('Glitchtrap: This is only the beginning.')
+    
+    def attack(self, target):
+        if 'sleep' in self.status:
+            print(f'{self.name} is asleep!')
+            sleep(self)
+        else:
+            print(f"{self.name} attacks {target.name}!")
+            n = random.randint(1, 100)
+            if n > 30 and n < 60: #28% chance to use this attack
+                print(f'{self.name} used Corrupt!')
+                if accuracy(50) == True:   
+                    self.damage += 20
+                    target.take_damage(self.damage)
+                    self.damage -= 20
+                    target.add_status('corrupt')
+                    corrupt(target)
+                else:
+                    print('The attack missed!')
+            if n > 15 and n < 31: #17% chance to use this attack
+                print(f'{self.name} used Digital Infiltration!')
+                if accuracy(30) == True:
+                    target.add_status('infiltrated')
+                    print(f"{self.name} infiltrated {target.name}'s system!")
+                else:
+                    print('The attack missed!')
+            if n >= 2 and n < 16: #14% chance to use this attack
+                print(f'{self.name} used System Overload!')
+                if accuracy(40) == True:
+                    self.damage += 40
+                    target.take_damage(self.damage) #somehow make it hit all players
+                    self.damage -= 40
+                else:
+                    print('The attack missed!')
+            if n >= 60: #40% chance to use this attack
+                print(f'{self.name} used Pixel Blast!')
+                if accuracy(70) == True:
+                    self.damage += 15
+                    target.take_damage(self.damage)
+                    self.damage -= 15
+                else:
+                    print('The attack missed!')
+            if n == 1:  #1% chance to use this attack
+                print(f'{self.name} hit the Griddy!') 
+                print('You were fascinated and stared in awe.')
 
 
 class Freddy:
@@ -297,7 +607,7 @@ class Freddy:
     def display_turn(self):
         print(f"It is {self.name}'s turn.")
 
-    def get_stats(self, target):
+    def get_stats(self):
         print(f"{self.name}'s stats")
         print(f"HP: {self.health}")
         #print(f"Light level: {self.light}")
@@ -305,10 +615,7 @@ class Freddy:
             print('Status: No statuses.')
         else:
             for status in self.status:
-                print(status)
-        back = input('Type Back to return.')
-        if back.lower() == 'back':
-            self.prompt_action(target)
+                print(f'Status: {status}')
 
     def prompt_action(self, target):
         print('1. Attack')
@@ -325,19 +632,32 @@ class Freddy:
 def sleep(target):
     if target.has_status('sleep'):
         if target.counter != 0:
+            print(f'{target} is already asleep!')
             target.turn_end()
         else:
             target.remove_status('sleep')
             print(f'{target.name} woke up!')
     else:
         target.counter += 3
+        print(f'{target} fell asleep!')
+
 
 def corrupt(target):
     if target.has_status('corrupt'):
         if target.counter != 0:
+            print(f'{target} is already corrupted!')
             target.turn_end()
         else:
             target.remove_status('corrupt')
             print(f'{target.name} stabilised itself!')
     else:
         target.counter += 1
+        print(f'{target} has been corrupted!')
+
+
+def infiltrated(target):
+    if target.has_status('infiltrated'):
+        print(f'{target} was infiltrated by Glitchtrap, causing them to lose a turn!')
+        target.remove_status('infiltrated')
+    else:
+        pass
