6d45201d03d2ff5f53d82ee74872e3554a95c010
DarkExcel <106753997+DarkExcel@users.noreply.github.com>
2023-08-17 06:58:53 +0000

    test.py

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 #test
 from main import game
 
+from game import MUDGame
+
+test = MUDGame()
+
 def test():
+    try:
+        test.run()
+    except:
+        print("The game cannot run lmao.")
+    
     pass
