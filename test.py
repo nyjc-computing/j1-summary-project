bf61bec836af758d387be0eb3b0cbb309b155dcc
DarkExcel <106753997+DarkExcel@users.noreply.github.com>
2023-08-17 08:30:20 +0000

    test

--- test.py
+++ test.py
@@ -4,14 +4,23 @@
 import data
 import game
 
-test = game.MUDGame()
+# test = game.MUDGame()
 
 def test():
 
+    # try:
+    #     data.start_menu()
+    # except:
+    #     print("Start Menu is screwed")
+    # else:
+    #     print("Start Menu is fine")
+
     try:
-        data.start_menu()
+        test_spawn_room = data.start_room()
+        print(test_spawn_room)
     except:
-        print("Start Menu is screwed")
+        print("i can't spawn cuz the room is bugged")
+    
     # try:
     #     test.run()
     # except:
