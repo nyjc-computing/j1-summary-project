79bbd3d97ed22519974d76002a988e4ee11f9ea5
56LogicErrorsAnd109SyntaxErrors <125201147+56LogicErrorsAnd109SyntaxErrors@users.noreply.github.com>
2023-08-20 14:56:02 +0000

    .

--- data.py
+++ data.py
@@ -3,23 +3,26 @@ import time
 
 #Rooms
 class Room:
-    def __init__(self, x = 2, y = 2, up = None, down = None, left = None, right = None, number = 0):
+    def __init__(self, type = 'normal', rooms_list = [], x = 2, y = 2, up = None, down = None, left = None, right = None, number = 0):
         #next rooms
+        self.type = type
+        self.rooms_list =  rooms_list + [self]
         self.up = up
         self.down = down 
         self.right = right
         self.left = left
         self.count = number
-        connections = random.randint(1, 3)
-        next_rooms = [self.up, self.down, self.left, self.right]
-        for room in next_rooms:
-            if room != None:
-                next_rooms.remove(room)
-        while connections != 0:
-            next_room = random.randint(0, len(next_rooms) - 1)
-            next_rooms[next_room] = 'closed'
-            next_rooms.pop(next_room)
-            connections = connections - 1
+        if self.count <= 10:
+            connections = random.randint(1, 3)
+            next_rooms = [self.up, self.down, self.left, self.right]
+            for room in next_rooms:
+                if room != None:
+                    next_rooms.remove(room)
+            while connections != 0:
+                next_room = random.randint(0, len(next_rooms) - 1)
+                next_rooms[next_room] = 'closed'
+                next_rooms.pop(next_room)
+                connections = connections - 1
             
         self.grid = Grid(x, y)
 
@@ -53,28 +56,28 @@ class Room:
                 print('It seems that this door is locked.')
             else:
                 prev = self
-                self.up = Room(up = prev, number=self.countRoom())
+                self.up = Room(rooms_list = self.rooms_list, up = prev, number=self.countRoom())
                 self = self.up
         if next.lower() == 's':
             if self.down == None:
                 print('It seems that this door is locked.')
             else:
                 prev = self
-                self.down = Room(down = prev, number=self.countRoom())
+                self.down = Room(rooms_list = self.rooms_list, down = prev, number=self.countRoom())
                 self = self.down
         if next.lower() == 'a':
             if self.left == None:
                 print('It seems that this door is locked.')
             else:
                 prev = self
-                self.left = Room(right = prev, number=self.countRoom())
+                self.left = Room(rooms_list = self.rooms_list, right = prev, number=self.countRoom())
                 self = self.left
         if next.lower() == 'd':
             if self.right == None:
                 print('It seems that this door is locked.')
             else:
                 prev = self
-                self.right = Room(left = prev, number=self.countRoom())
+                self.right = Room(rooms_list = self.rooms_list, left = prev, number=self.countRoom())
                 self = self.right
     def current_room(self) -> 'Room':
         '''
@@ -85,6 +88,7 @@ class Room:
     def countRoom(self):
         self.count += 1
         return self.count
+
 
 def start_room():
     """Instantiates a spawn room"""
