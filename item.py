27a1f52c6219e53d1898e5e8d5b85c568222318f
mc4706 <141309318+mc4706@users.noreply.github.com>
2023-08-21 09:04:15 +0000

    hi

--- item.py
+++ item.py
@@ -15,6 +15,7 @@ class Weapon:
     """
 
     def __init__(self, name: str, description: str, attack: int) -> None:
+        self.type = 'Weapon'
         self.name = name
         self.description = description
         self.attack = attack
@@ -22,3 +23,29 @@ class Weapon:
     def __repr__(self) -> str:
         return print(f"Item{self.name}")
 
+class Potion:
+    """
+    creates the potion
+
+    Attribute
+    ---------
+    + name: str
+    + description: str
+    + attack: int | float
+    + heal: int | float
+
+    Method
+    ------
+    
+    
+    """
+
+    def __init__(self, name: str, description: str, attack: int, heal: int) -> None:
+        self.type = 'potion'
+        self.name = name
+        self.description = description
+        self.attack = attack
+        self.heal = heal
+
+    def __repr__(self) -> str:
+        return print(f"Item{self.name}")
