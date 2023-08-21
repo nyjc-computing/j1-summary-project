7ec3ced73463895be1beb7a85f06d5c702d0d556
stanblob <125365790+stanblob@users.noreply.github.com>
2023-08-21 08:37:41 +0000

    ......

--- data.py
+++ data.py
@@ -794,9 +794,9 @@ class Boss(Creature):
     -- METHODS --
     """
     def __init__(self):
-        super().__init__("King Warden", 100, 10):
+        super().__init__("King Warden", 100, 10)
 
-    def heal(hp: int) -> None:
+    def heal(self, hp: int) -> None:
         self.hitpoints = min(self.hitpoints + hp, self.maxhp)
         
 def random_creature() -> "Creature":
