114810533d8c6bd29b3213d8f1a44ff361eb4b2e
yxlyx <85774423+yxlyx@users.noreply.github.com>
2023-08-24 07:00:53 +0000

    k

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
+#File for QAE
+# for each critical method, test the method (template is test_attack(), think abt what the mtd does/outcome aft method is run) !!!!!
 
+from data import Labyrinth
+
+def test_lbr_init():
+    lb = Labyrinth()
+    
+# test_lbr_init()
+lb = Labyrinth()
+lb.generate()
+
+from game import MUDGame
+
+mg = MUDGame()
+
+def test_attack():
+    """Check that the attack() method"""
+    mg.attack()
+    assert mg.steve.isdead() or mg.creature.isdead()
+
+def test_movesteve():
+    befpos = mg.maze.get_current_pos()
+    mg.movesteve()
+    aftpos = mg.maze.get_current_pos()
+    if befpos == aftpos:
+        raise RuntimeError("After movesteve() was run, Steve did not move.")
