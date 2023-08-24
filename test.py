503266c877757d4825129cf478010ce24f0942fa
yxlyx <85774423+yxlyx@users.noreply.github.com>
2023-08-24 05:51:00 +0000

    test

--- test.py
+++ test.py
@@ -1,17 +1,22 @@
 #File for QAE
+# for each critical method, test the method (template is test_attack(), think abt what the mtd does/outcome aft method is run) !!!!!
 
 from data import Labyrinth
 
 def test_lbr_init():
-    try:
-        lb = Labyrinth()
-    except:
-        import pdb; pdb.set_trace()
-
+    lb = Labyrinth()
+    
+# test_lbr_init()
 # lb = Labyrinth()
 # lb.generate()
 
 from game import MUDGame
 
 mg = MUDGame()
-mg.attack()
+
+def test_attack():
+    """Check that the attack() method"""
+    mg.attack()
+    assert mg.steve.isdead() or mg.creature.isdead()
+
+mg.item_found()
