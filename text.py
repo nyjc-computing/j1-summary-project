c4f474e780ea25e3d401688bbea94ce43ed4adb0
Niubiable1 <125109794+Niubiable1@users.noreply.github.com>
2023-08-24 06:16:25 +0000

    better ui

---
+++ text.py
@@ -0,0 +1,29 @@
+divider = "====================================================="
+
+intro = """Welcome to Valorant.
+        
+Your team all got killed by the Ranked Demon Reyna on the enemy team, 
+you need to clutch. 
+
+The Reyna will certainly one tap you the second you get into an encounter so the only way you're going to win this is by picking up enough shields (total 300hp) to not instantly die. 
+
+Shield orbs can be found randomly throughout the map, and add 50hp to your character. 
+
+There is still enemy utility scattered around the map, each piece you encounter will deduct 30hp from your character. 
+
+You also have an agent ability, which will hopefully give you an edge over this no-lifer. Using an ability does not pass your turn. 
+
+Remember, CLUTCH OR GAE!!
+
+====================================================="""
+
+agent = """CHOOSE YOUR AGENT:
+ 
+[1] Jett: Dash into an adjacent room if you would otherwise die (does not refresh)
+    
+[2] Sova: Scan an adjacent room for information (cooldown: 2 turn)
+    
+[3] Omen: Move to any room on the map (cooldown: 5 turns)
+    
+[4] Sage: Block a path from your current room to an adjacent one permanently (cooldown: 3 turns)
+====================================================="""
