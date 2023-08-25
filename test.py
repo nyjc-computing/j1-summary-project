6c9e141bd8a99c6ae88a8168b96977360cfe9b8b
DarkExcel <106753997+DarkExcel@users.noreply.github.com>
2023-08-25 05:13:11 +0000

    .

--- test.py
+++ test.py
@@ -19,34 +19,38 @@ import game
 Test the Normal Room and Grid class for errors.
 ===============================================
 '''
-# test_grid = data.Grid("normal", 2, 2)
-# test_room = data.Room()
-        
-# try:
+try:
+    test_grid = data.Grid("normal", 2, 2)
+except:
+    print("Grid cannot be instantiated")
+try:
+    test_room = data.Room()
+except:
+    print("Room cannot be instantiated")
     
-#     if test_room.grid.get_position() != [2, 2]:
-#         print("Grid positioning does not work properly.")
-#     else:
-#         print("Grid position is ok.")
+try:
     
-# except:
-#     print("Grid cannot instantiate (the function does not work).")
-# test_room.grid.move([0,0])
+    if test_room.grid.get_position() != [2, 2]:
+        print("Grid positioning does not work properly.")
+    else:
+        print("Grid position is ok.")
+    
+except:
+    print("Grid cannot instantiate (the function does not work).")
+test_room.grid.move([0,0])
 
-# for x in range(0, 5):
-#     for y in range(0, 5):
-#         pos = [x, y]
-#         print(pos)
-#         try:
-#             if test_room.grid.is_encounter():
-#                 print(test_room.grid.get_enemies())
-#         except:
-#             print("is_encounter() does not work")
-#         test_room.grid.move(pos)
+for x in range(0, 5):
+    for y in range(0, 5):
+        pos = [x, y]
+        print(pos)
+        try:
+            if test_room.grid.is_encounter():
+                print(test_room.grid.get_enemies())
+        except:
+            print("is_encounter() does not work")
+        test_room.grid.move(pos)
 
-# test_room.next_room('w')
-
-# print("Room and Grid instantiated with no issues.")
+print("Room and Grid instantiated with no issues.")
 
 
 
@@ -58,16 +62,16 @@ Test the start_room function for errors.
 ========================================
 '''
 
-# try:
-#     test_spawn_room = data.start_room()
-#     if test_spawn_room.__class__.__name__ == "Room":
-#         print("Spawn Room is a Room Object")
-#     else:
-#         raise Exception("Spawn Room created is not a Room Object")
-# except:
-#     raise Exception("Spawn Room function does not run.")
-# else:
-#     print("Spawn Room instantiated with no issues.")
+try:
+    test_spawn_room = data.start_room()
+    if test_spawn_room.__class__.__name__ == "Room":
+        print("Spawn Room is a Room Object")
+    else:
+        raise Exception("Spawn Room created is not a Room Object")
+except:
+    raise Exception("Spawn Room function does not run.")
+else:
+    print("Spawn Room instantiated with no issues.")
 
 
 
@@ -103,14 +107,11 @@ Test the Grid class for errors.
 # print("Grid instantiated with no issues.")
 
 
-        
-        
 test_freddy = data.Freddy()
-test_enemy = data.GB()
-while test_enemy.is_defeated() == False:
-    test_freddy.attack(test_enemy, 1)
-    test_freddy.attack(test_enemy, 2)
-    test_freddy.attack(test_enemy, 3)
+test_BB = data.BB()
+        
+        
+    
         
     
     # try:
