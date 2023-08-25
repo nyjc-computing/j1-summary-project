364f125e8eed4d3b5738b25872a0802949b76c12
stanblob <125365790+stanblob@users.noreply.github.com>
2023-08-25 07:02:45 +0000

    idk some stuff

--- game.py
+++ game.py
@@ -95,6 +95,8 @@ class MUDGame:
                 print(f"The {creature.name} has healed itself.")
             else:
                 print(f"The {creature.name} has dealt {damage} damage on you.")
+        if room.creature.isdead():
+            room.set_creature_None()
         
 
         
