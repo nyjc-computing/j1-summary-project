6e69e75eec00180d723c0cd763a7678e738a9088
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2024-07-26 07:32:27 +0000

    ;(

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Import statements
-
+import random
+class William:
+    def shout(self):
+        print(''.join([i.upper() if random.randint(0,1) else i.lower() for i in "Hong Wei Lian"]))
 
 if __name__ == "__main__":
-    print("hi")
-    print('bye')
-    print("uwu")
+    william = William()
+    william.shout()
