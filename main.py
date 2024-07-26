7ac88c561cebabd10f28eac64375ca3d876e75ca
corinne <160467276+coryxn@users.noreply.github.com>
2024-07-26 08:26:34 +0000

    26/7

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('uwu')
+import random
+from Character import *
+
+
+class Room:
+
+  def __init__(self, name):
+    self.name = name
+    self.enemy_num = random.randint(2, 5)
+
+  def fight(self):
+    pass
