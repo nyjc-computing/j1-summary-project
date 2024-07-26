d582cae95a583bd71a159fc5ca5462721ebb892b
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2024-07-26 07:57:04 +0000

    .

---
+++ character.py
@@ -0,0 +1,7 @@
+class Character():
+
+    def __init__(self, type, health, strength, items):
+        self.type = type
+        self.health = health
+        self.strength = strength
+        self.items = items
