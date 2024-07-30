9fb1792ea71d86985696ac9776276d6a52c4d8fc
corinne <160467276+coryxn@users.noreply.github.com>
2024-07-30 03:01:33 +0000

    huh

--- room.py
+++ room.py
@@ -1,8 +1,23 @@
+# import character
+import csv
+
 class Room:
 
-def __init__(self, name, direction):
-  self.name = name
-  self.enemy_num = random.randint(2, 5)
+    def __init__(self, name, next, enemy_num):
+        self.name = name
+        self.next = next
+        self.enemy_num = enemy_num
 
-def fight(self):
-  pass
+    def display(self):
+        print(f'name: {self.name}, next rooms: {self.next}, number of enemies: {self.enemy_num}')
+
+with open('room_data.csv', 'r') as f:
+    header = f.readline().strip().split(',')
+    for room in f.readlines():
+        room = room.strip().split(',')
+        print(room)
+#         name = room[0]
+#         name = Room(room[0], room[1], room[2], room[3])
+#         name.display()
+
+# bedroom.display()
