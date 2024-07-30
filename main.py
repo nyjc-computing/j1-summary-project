f83b6fdc9de2ea21d7f40a32635593210c8d32f5
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2024-07-30 02:27:01 +0000

    .

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 
 if __name__ == "__main__":
   pass
+
+"""
+class game:
+  def __init__(self):
+    self.player = None
+    self.rooms = []
+    self.now = 0
+
+  def initialise(self):
+    self.player = Player()
+    self.rooms.append(Room("Toilet"))
+"""
