fbd4fddd365757055cacf2117228409c7a8a0aea
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-08-01 06:41:31 +0000

    Game loop v1

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # Import statements
 import mud
 
+import data
+
 if __name__ == "__main__":
     game = mud.Game()
     mud.welcome()
     players = mud.init_players()
-    
+    # Game loop
+    while not game.is_gameover():
+        ...
+    game.epilogue()
