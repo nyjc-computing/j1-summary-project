b316f01d8de5383446eb16786b3a401d0f0964d7
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-08-01 07:44:23 +0000

    Update game loop (v2)

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import data
 if __name__ == "__main__":
     game = mud.Game()
     mud.welcome()
-    players = mud.init_players()
-    # Game loop
+    player = data.create_player()
+    game.add_player(player)
     while not game.is_gameover():
-        ...
-    game.epilogue()
+        choices = game.get_options()
+        choice = data.prompt_player_choice(choices)
+        actions = game.get_actions(choice)
+        game.execute(actions)
+        data.display(game.status())
+    mud.epilogue()
