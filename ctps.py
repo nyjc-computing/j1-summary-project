359c06c7a84f97046c7c038fdecee16fdac9c5f8
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2024-08-02 07:11:40 +0000

    .

--- ctps.py
+++ ctps.py
@@ -1,12 +1,25 @@
+from character import *
+
 class Game:
-  def __init__(self):
-      self.player = None
-      self.rooms = []
-      self.now = 0
+    def __init__(self):
+        self.player = None
+        self.princess = None
+        self.rooms = []
+        self.now = 0
 
-  def setup(self):
-      self.player = Player()
-      self.rooms.append(Bedroom())
+    def setup(self):
+        self.player = Player(1, 1, [])
+        self.rooms.append(Bedroom())
 
-  def isover(self):
-      return self.player.isdead()
+    def isdead(self):
+        return self.player.isdead()
+
+    def isover(self):
+        return self.isdead()
+
+
+
+    def next_room(self):
+        self.now += 1
+    
+
