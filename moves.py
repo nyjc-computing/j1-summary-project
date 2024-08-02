4ce2adcf4b5a902f00f42b1e42fa6962cece6e1a
kenneth <88260663+its-kenneth@users.noreply.github.com>
2024-08-02 08:25:38 +0000

    i did some stuff

---
+++ moves.py
@@ -0,0 +1,10 @@
+class Moves:
+    def __init_(self, name, multiplier):
+        self.name = name
+        self.multiplier = 
+        self.power_limit = 
+        self.current_power = 
+
+'''
+config
+'''
