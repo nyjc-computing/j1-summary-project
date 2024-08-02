a8f73e2fb0332931b5956a0eaec0f1f311587f3e
AngWilliamLee <160611372+AngWilliamLee@users.noreply.github.com>
2024-08-02 08:28:17 +0000

    added interface calss and script

--- interface.py
+++ interface.py
@@ -1,4 +1,18 @@
-class Interface:
+import script
 
-    def start_menu():
-        
+class interface:
+
+    def start_menu(self):
+        print(script.start_menu['title'])
+        [print(f"{num+1}: {value}") for num, value in enumerate(script.start_menu["options"])]
+        print(script.prompt)
+
+    def exit_screen():
+        print()
+
+
+
+
+
+UI = interface()
+UI.start_menu()
