fb83553eb10764393c7a7dab6cfb1e2979d2bae3
corinne <160467276+coryxn@users.noreply.github.com>
2024-08-02 08:05:49 +0000

    .

--- room.py
+++ room.py
@@ -11,3 +11,16 @@ class Room:
         self.enemy_num -= 1
         if self.enemy_num <= 0:
              print(f'Enemies in {self.name} have all been defeated!')
+
+
+
+
+def get_choice(self):
+    print(f'You are in {self.get_now_room()}')
+    print(f'-Choices- \n 1. next room: {self.nextroom} \n 2. prev room: {self.prev_room} \n 3. look around')
+    choice = input("type your choice here: ")
+    if choice not in '123':
+        print('error')
+    elif choice == 1:
+        pass
+    
