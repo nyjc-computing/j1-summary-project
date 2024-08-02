8dad1a955b8885e14385b71cd18ca7ec1910a4fb
GohJianLin <160487674+GohJianLin@users.noreply.github.com>
2024-08-02 07:35:00 +0000

    Partially finished Gear

--- character.py
+++ character.py
@@ -12,52 +12,68 @@ class Player:
         self.items = {}
         self.backpack_size = slots
         #Gears
-        self.helmet = (None)
-        self.chestplate = (None)
-        self.leggings = (None)
-        self.boots = (None)
-        self.accessories = (None)
+        self.gears = {'Helmet': None, 'Chestplate': None, 'Leggings': None, 'Boots': None, 'Weapons': None, 'Accessories': None}
         
     def __repr__(self):
         return f"Name: {self.name}"
+        
     #Backpack
-    def store(self, name, object):
+    def store(self, item):#item is an object
+        name = item.name
         if name in self.items: #same type items stack
-            self.items[name].num += object.num
-            print(f'{name} * {object.num} has been stored')
-            return
+            self.items[name].num += item.num
+            print(f'{name} * {item.num} has been stored')
+            return True
 
         if len(self.items) >= self.backpack_size: #backback full
             print("Backpack is full!")
-            return
+            return False
 
-        self.items[name] = object #normal store with sufficient space
-        print(f'{name} * {object.num} has been stored.')
-        return
+        self.items[name] = item #normal store with sufficient space
+        print(f'{name} * {item.num} has been stored.')
+        return True
 
     def display(self):
         lst = [i for i in self.items.keys()]
         disp = ', '.join(lst) #all items in backpack
         return disp
 
-    def check(self, item):
-        if item in self.items.keys():
-            print(f'Name: {item}')
-            print(f'Amount:{self.items[item].num}')
-            print(f'Description:{self.items[item].desc}')t
-            return
+    def check(self, item_name):
+        if item_name in self.items.keys():
+            print(f'Name: {item_name}')
+            print(f'Amount:{self.items[item_name].num}')
+            print(f'Description:{self.items[item_name].desc}')
+            return True
         print('Item not in Backpack')
-        return
-    #Gears
-    def equip(self, gear):
-        if gear not in self.items:
-            print('You don't have that gear!')
+        return False
         
-class Object:
-    def __init__(self, num, desc):
+    #Gears
+    def equip(self, gear: 'Item'):
+        if gear.name not in self.items:
+            print("You don't have that gear!")
+            return False
+        #if that section is full, say you have it on
+        pass
+
+    def unequip(self, section):
+        if self.gears[section] is None:
+            print('Nothing is equipped there.')
+            return False
+            
+        if self.store(self.gears[section]) is False:
+            print(f'Backpack Full! {section} cannot be unequipped!')
+            return False
+            
+        self.store(self.gears[section])
+        self.gears[section] = None
+        print(f'{self.gears[section].name} unequipped')
+        return True
+        
+class Item:
+    def __init__(self, num, desc, name):
+        self.name = name
         self.num = num
         self.desc = desc
-
 
 class Enemy:
     def __init__(self, type):
