5447cf24b2394f7a90b548d848524b0e84086654
Chilliken <74310036+Chilliken@users.noreply.github.com>
2024-08-02 07:49:25 +0000

    entity 3

--- entities.py
+++ entities.py
@@ -6,7 +6,6 @@ class Entity():
         self.health = health
         self.aura = aura
         self.position = position
-        self.items = []
 
     def get_name(self):
         return self.name
@@ -14,23 +13,10 @@ class Entity():
     def get_health(self):
         return self.health
 
-    def get_aura(self):
-        return self.aura
-
     def set_health(self,health):
         self.health = health
 
-    def set_aura(self,aura):
-        self.aura = aura
-
-    def add_item(self,item):
-        self.items.append(item)
-
-    def remove_item(self,item):
-        self.items.remove(item)
-
-    def move(self,position):
-        move = input()
+    def move(self, position, move):
         if move == "W":
             position[1] += 1
         elif move == "A":
@@ -45,6 +31,59 @@ class Entity():
 
 
 class Player(Entity):
-    pass
+
+    def __init__(self, name, health, aura, position,inventory = []):
+        super().__init__(name, health, aura, position)
+        self.inventory = inventory
+
+    def get_aura(self):
+        return self.aura
+
+    def set_aura(self,aura):
+        self.aura = aura
+
+    def add_item(self,item):
+        self.inventory.append(item)
+
+    def remove_item(self,item):
+        self.inventory.remove(item)
+
+    def get_inventory(self):
+        return self.inventory
+
+    
+class Monster(Entity):
+    
+    def __init__(self, name, health, aura, position, damage, description, inventory = []):
+        super().__init__(name, health, aura, position)
+        self.damage = damage
+        self.description = description
+        self.inventory = inventory
+        
+
+    def pickup_item(self,item):
+        self.inventory.append(item)
+
+    def get_inventory(self):
+        return self.inventory
+    
+        
+class Creature(Entity):
+
+    def __init__(self, name, health, aura, position, damage, description):
+        super().__init__(name, health, aura, position)
+        self.damage = damage
+        self.description = description
+
+
+    def get_damage(self):
+        return self.damage
+
+    def set_damage(self, damage):
+        self.damage = damage
+
+        
+        
+    
     
     
