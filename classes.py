59d4a94c490f44e51aab63b5b8ea177deb3406a5
ppannawitt <106550775+ppannawitt@users.noreply.github.com>
2024-08-02 07:05:18 +0000

    add class. game

--- classes.py
+++ classes.py
@@ -1,3 +1,38 @@
+class Gears:
+    def __init__(self):
+        self.helmet = (None)
+        self.chestplate = (None)
+        self.leggings = (None)
+        self.boots = (None)
+        self.accessories = (None)
+        pass
+        
+class Backpack: #store, display, check, destroy
+    def __init__(self, slots):
+        self.items = {}
+        self.backpack_size = slots
+
+    def store(self, name, object):
+        if name in self.items:
+            self.items[name].num += object.num
+            print(f'{name} * {object.num} has been stored')
+            return
+            
+        if len(self.items) >= self.backpack_size:
+            print("Backpack is full!")
+            return
+        self.items[name] = object
+        print(f'{name} * {object.num} has been stored.')
+        return
+        
+    def display(self):
+        lst = [i for i in self.items.keys()]
+        disp = ', '.join(lst)
+        return disp
+
+    def check(self, item):
+        pass
+
 class Player:
     def __init__(self, name):
         self.name = str(name)
@@ -12,17 +47,77 @@ class Player:
     def __repr__(self):
         return f"Name: {self.name}"
 
+class Backpack:
+    pass
+        
 
 class Object:
     def __init__(self, num, desc):
         self.num = num
         self.desc = desc
 
+
 class Enemy:
     def __init__(self, type):
-        if type == "Brute":
+        if type:
             self.health, self.attack, self.defense = 10, 2, 1
-        else:
+        else:  
             self.health, self.attack, self.defense = 5, 1, 0
     def attack(self, player_obj):
         pass
+
+
+from random import randint
+class Game:
+    def __init__(self, name):
+        self.n = 10
+        self.e = 5
+        self.x = 0, self.y = 0
+        self.enemies = {}
+        map = [['.'for i in range(10)]]
+        player = Player(name)
+    def printmap(self):
+        for i in range(self.n):
+            print(''.join(map[i]))
+
+    def random_map():
+        for i in range(5):
+            co = (randint(0, self.n-1), randint(0, self.n-1))
+            if co in dic:
+                i-=1
+                continue
+            self.enemies[co] = True
+
+    def play():
+        self.random_map()
+        while (self.x!=self.n-1 and self.y!=self.n-1):
+            move = input()
+            if move=='w' and self.x > 0:
+                self.x-=1
+            elif move=='a' and self.y > 0:
+                self.y+=1
+            elif move=='s' and self.x < self.n:
+                self.x-=1
+            elif move=='d' and self.y < self.n:
+                self.y-=1
+            else:
+                print("Invalid Move!")
+                continue
+            self.printmap()
+            if ((self.x, self.y) in self.enemies):
+                print("The ememy has been summoned!")
+                if self.fight(self.enemies[(self.x, self.y)]):
+                    self.lose()
+                    return
+        self.win()
+        return
+            
+            
+    def fight(self, enemy):
+        return False
+
+    def win(self):
+        print("You have arrived safely. Well done!")
+    def lose(self):
+        print("Game over")
+        
