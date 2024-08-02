869a5a26a43845d1fa5897558ea82ee102e8197b
GohJianLin <160487674+GohJianLin@users.noreply.github.com>
2024-08-02 07:10:57 +0000

    Finished backpack

--- Enemy.py
+++
@@ -1,9 +0,0 @@
-class Enemy:
-    def __init__(self, type):
-        if type == "Brute":
-            self.health, self.attack, self.defense = 10, 2, 1
-        else:
-            self.health, self.attack, self.defense = 5, 1, 0
-
-    def fight(self, player_obj):
-        pass
---
+++ character.py
@@ -0,0 +1,67 @@
+class Player:
+    def __init__(self, name, slots):
+        self.name = str(name)
+        self.health = 10
+        self.defense = 0
+        self.attack = 1
+        self.dodge = 0.05 #5%
+        self.speed = 1
+        self.crit_chance = 0.05 
+        self.crit_dmg = 2 #200%
+        #Backpack
+        self.items = {}
+        self.backpack_size = slots
+        #Gears
+        self.helmet = (None)
+        self.chestplate = (None)
+        self.leggings = (None)
+        self.boots = (None)
+        self.accessories = (None)
+        
+    def __repr__(self):
+        return f"Name: {self.name}"
+    #Backpack
+    def store(self, name, object):
+        if name in self.items: #same type items stack
+            self.items[name].num += object.num
+            print(f'{name} * {object.num} has been stored')
+            return
+
+        if len(self.items) >= self.backpack_size: #backback full
+            print("Backpack is full!")
+            return
+
+        self.items[name] = object #normal store with sufficient space
+        print(f'{name} * {object.num} has been stored.')
+        return
+
+    def display(self):
+        lst = [i for i in self.items.keys()]
+        disp = ', '.join(lst) #all items in backpack
+        return disp
+
+    def check(self, item):
+        if item in self.items.keys():
+            print(f'Name: {item}')
+            print(f'Amount:{self.items[item].num}')
+            print(f'Description:{self.items[item].desc}')t
+            return
+        print('Item not in Backpack')
+        return
+    #Gears
+    def equip(self, gear):
+        if gear not in self.items:
+            print('You don't have that gear!')
+        
+class Object:
+    def __init__(self, num, desc):
+        self.num = num
+        self.desc = desc
+
+
+class Enemy:
+    def __init__(self, type):
+        if type == "Brute":
+            self.health, self.attack, self.defense = 10, 2, 1
+        else:
+            self.health, self.attack, self.defense = 5, 1, 0
