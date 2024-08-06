5912b55683ebb7940ee310f2f681133fadbb5bee
Steve Nguyen <154491875+majesticmist@users.noreply.github.com>
2024-08-06 03:31:21 +0000

    Created factory functions

---
+++ data.py
@@ -0,0 +1,19 @@
+import room
+import character
+
+
+def createRooms() -> list[room.Room]: 
+    """
+    Must follow order of room: Dungeon, Kitchen, Hall, Toilet, Bedroom
+    Store Name & Number of enemies
+    """
+    pass
+    
+
+
+def createPlayer() -> character.Player:
+    pass
+
+
+def createPrincess() -> character.Princess:
+    pass
