f02242a48a97ab87deddc82f7274666490a75bcd
Steve Nguyen <154491875+majesticmist@users.noreply.github.com>
2024-08-06 03:35:31 +0000

    Implemented factory functions

--- data.py
+++ data.py
@@ -2,18 +2,28 @@ import room
 import character
 
 
-def createRooms() -> list[room.Room]: 
+def createRooms() -> list[room.Room]:
     """
     Must follow order of room: Dungeon, Kitchen, Hall, Toilet, Bedroom
     Store Name & Number of enemies
     """
-    pass
+    list_of_rooms = [
+        room.Room("Dungeon", 3),
+        room.Room("Kitchen", 3),
+        room.Room("Hall", 3),
+        room.Room("Toilet", 3),
+        room.Room("Bedroom", 3)
+    ]
+
+    return list_of_rooms
     
 
 
 def createPlayer() -> character.Player:
-    pass
+    
+    return character.Player(1,1, [])
 
 
 def createPrincess() -> character.Princess:
-    pass
+
+    return character.Princess(1,1)
