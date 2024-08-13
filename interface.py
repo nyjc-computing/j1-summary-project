813053868f83cb821969c5421fd10fff4b35d70e
AngWilliamLee <160611372+AngWilliamLee@users.noreply.github.com>
2024-08-13 02:49:36 +0000

    interface and script

--- interface.py
+++ interface.py
@@ -12,11 +12,19 @@ class interface:
         
         [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
 
-    # def exit_screen():
-    #     print()
+    def exit_screen(self):
+        print(script.exit_screen['message'])
+
+    def toilet_menu(self):
+        print(script.start_menu['title'])
+        [print(f"{num+1}: {value}") for num, value in enumerate(script.toilet_menu["options"])]
+        choice = int(input(script.prompt))
+        return script.toilet_menu['options'][choice - 1]
+
+    
 
 
 
 UI = interface()
-print(UI.start_menu())
+print(UI.toilet_menu())
 
--- script.py
+++ script.py
@@ -5,10 +5,23 @@ start_menu = {
 }
 
 exit_screen = {
-
-
+'message': 'See you next time!'
 }
 
 room_menu = {
 'options': ['Look around', 'Move to next room', 'Move to previous room']
+}
+
+story_menu = {
+'message': 'Once upon a time, there was a powerful kingdom called “The Light Nation “. ', 'options': ['Previous Line', 'Next Line']
+   
+}
+
+toilet_menu = {
+'message': 'You are currently in the toilet.', 'options': ['Skibidi', 'Investigate (count how many guards in the toilet)', 'Fight', ' Use items', 'Move to another room (this one idk how its gonna work _ need clarification guys)']
+}
+
+combat_menu = {
+'message': 'You have encountered ____', 'options': ['Roll a dice']
+
 }
