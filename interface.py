17925a8281740921f73e43fd796858dc3b2da040
AngWilliamLee <160611372+AngWilliamLee@users.noreply.github.com>
2024-08-16 07:45:54 +0000

    k

--- interface.py
+++ interface.py
@@ -11,7 +11,11 @@ class Interface:
         [print(f"{num+1}: {value}") for num, value in enumerate(script.start_menu["options"])]
         choice = int(input(script.prompt))
         return script.start_menu['options'][choice - 1]
-    
+
+    def combat_menu(self, turn):
+        print(f"Your health: {player_health}")
+        print(f"Enemy health: {enemy_health}")
+        
     def dungeon_menu(self):
         [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
 
@@ -29,11 +33,5 @@ class Interface:
 
     def exit_screen(self):
         print(script.exit_screen['message'])
-
-    def toilet_menu(self):
-        print(script.toilet_menu['message'])
-        [print(f"{num+1}: {value}") for num, value in enumerate(script.toilet_menu["options"])]
-        choice = int(input(script.prompt))
-        return script.toilet_menu['options'][choice - 1]
 
 
