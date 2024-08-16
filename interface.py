916aa0a9c90c321cea57b3ed3add2165fa5fbea3
AngWilliamLee <160611372+AngWilliamLee@users.noreply.github.com>
2024-08-16 08:09:11 +0000

    v

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import script
-
+import room
+import ctps
 
 class Interface:
     def prompt(self):
@@ -12,26 +13,29 @@ class Interface:
         choice = int(input(script.prompt))
         return script.start_menu['options'][choice - 1]
 
-    def combat_menu(self, turn):
-        print(f"Your health: {player_health}")
-        print(f"Enemy health: {enemy_health}")
+    def combat_menu(self):
+        print(f"Your health: {ctps.get_player_health()}")
+        print(f"Enemy health: {room.get_enemy_health()}")
         
     def dungeon_menu(self):
         [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
 
     def kitchen_menu(self):
-    [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
+        [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
 
     def hall_menu(self):
-    [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
+        [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
 
     def toilet_menu(self):
-    [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
+        [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
 
     def bedroom_menu(self):
-    [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
+        [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
 
     def exit_screen(self):
         print(script.exit_screen['message'])
+
+m = Interface()
+m.combat_menu()
 
 
