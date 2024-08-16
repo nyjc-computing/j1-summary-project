aed903bf1701d7a0e16da4d340652d396182b47c
AngWilliamLee <160611372+AngWilliamLee@users.noreply.github.com>
2024-08-16 07:18:42 +0000

    update

--- interface.py
+++ interface.py
@@ -13,26 +13,15 @@ class Interface:
         return script.start_menu['options'][choice - 1]
     
     def room_menu(self):
-        
         [print(f"{num+1}: {value}") for num, value in enumerate(script.room_menu["options"])]
-        
-        [
-            print(f"{num+1}: {value}")
-            for num, value in enumerate(script.start_menu["options"])
-        ]
 
     def exit_screen(self):
         print(script.exit_screen['message'])
 
     def toilet_menu(self):
-        print(script.start_menu['title'])
+        print(script.toilet_menu['message'])
         [print(f"{num+1}: {value}") for num, value in enumerate(script.toilet_menu["options"])]
         choice = int(input(script.prompt))
         return script.toilet_menu['options'][choice - 1]
 
-
-
-
-UI = interface()
-print(UI.toilet_menu())
 
