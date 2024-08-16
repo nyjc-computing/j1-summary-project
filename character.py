bdb5062451afc24037ce4a2eb26809f15f5a43c7
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2024-08-16 08:18:45 +0000

    .

--- character.py
+++ character.py
@@ -10,7 +10,7 @@ class Character:
         """
         Deal damage to another character object
         """
-        character.receive_damage(self.strength)
+        character.receive_damage(1)
         # print("Die")
 
     def receive_damage(self, damage):
@@ -32,16 +32,16 @@ class Character:
 class Player(Character):
 
     def __init__(self, health, strength, items=[]):
-        super().__init__("Player", health, strength, items)
+        super().__init__("Player", health, items)
 
 
 class Soldier(Character):
 
-    def __init__(self, health, strength):
-        super().__init__("Soldier", health, strength)
+    def __init__(self, health):
+        super().__init__("Soldier", health)
 
 
 class Princess(Character):
 
-    def __init__(self, health, strength):
-        super().__init__("Princess", health, strength)
+    def __init__(self, health):
+        super().__init__("Princess", health)
