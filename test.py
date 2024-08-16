b467f493f79f9a732089699fbf1e7a9ebbcd42ae
xint0n9.l <160615573+xint0n9@users.noreply.github.com>
2024-08-16 07:17:56 +0000

    first v

--- test.py
+++ test.py
@@ -1,36 +1,12 @@
-import contextlib
-import subprocess
-import sys
-import unittest
+from game import Game
 
-# limit number of retries
-MAX_RETRIES = 5
-retries = 0
+def test_1():
+    game = Game()
+    game.set_player_name("testplayer")
+    here = game.current_room()
+    game.move("left")
+    game.move("right")
+    assert game.current_room() == here
 
-# Force reload of autograding submodule
-# This allows it to be updated even after students have accepted an assignment
-# It would otherwise require them to delete and re-accept the assignment
-# (or alternatively update the submodule manually)
-def unload():
-    global autograding
-    if "autograding" in sys.modules:
-        del sys.modules["autograding"]
-    with contextlib.suppress(NameError):
-        del autograding
-
-unload()
-subprocess.run(["git", "submodule", "update", "--init", "--remote"])
-import autograding
-from autograding.case import FuncCall, InOut
-
-
-class TestSF(autograding.TestInputOutput):
-    def setUp(self):
-        self.testcases = [
-            InOut(input="playername", output="hello")
-        ]
-
-
-if __name__ == '__main__':
-    unittest.main()
-
+def test_2():
+    pass
