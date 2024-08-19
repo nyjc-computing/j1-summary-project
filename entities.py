1fa4b588fee819703428c3ac7047386aacb0d4f9
Chilliken <74310036+Chilliken@users.noreply.github.com>
2024-08-19 10:31:03 +0000

    entities

--- entities.py
+++ entities.py
@@ -20,50 +20,50 @@ class Entity:
 
     def take_damage(self, damage):
         self.health -= damage
-    
+
     def get_position(self):
         return self.position
 
     def dead(self):
         return self.health <= 0
-    
+
     def move(self, move):
-        
+
         if move in "Ww":
             if self.get_position()[1] <= 1:
                 return "invalid"
             else:
                 self.position[1] -= 1
                 return self.get_position()
-                
+
         elif move in "Aa":
             if self.get_position()[0] <= 1:
                 return "invalid"
             else:
                 self.position[0] -= 1
                 return self.get_position()
-            
+
         elif move in "Ss":
             if self.get_position()[1] >= 20:
                 return "invalid"
             else:
                 self.position[1] += 1
                 return self.get_position()
-    
-            
+
+
         elif move in "Dd":
             if self.get_position()[0] >= 20:
                 return "invalid"
             else:
                 self.position[0] += 1
                 return self.get_position()
-            
-    
+
+
 
 
 class Player(Entity):
 
-    def __init__(self, name, health, aura, position, inventory = {}):
+    def __init__(self, name, health, aura, position, inventory = []):
         super().__init__(name, health, position)
         self.inventory = inventory
         self.aura = aura
@@ -76,9 +76,9 @@ class Player(Entity):
 
     def gain_aura(self,aura_gained):
         self.aura += aura_gained
-    
+
     def add_item(self,item):
-        self.inventory[len(self.inventory) + 1] = item
+        self.inventory.append(item)
 
     def remove_item(self,item_index):
         self.inventory.pop(item_index)
@@ -96,7 +96,7 @@ class Player(Entity):
     def get_inventory(self):
         return self.inventory
 
-        
+
 class Monsters(Entity):
 
     def __init__(self, name, health, position, damage, description):
@@ -113,14 +113,14 @@ class Monsters(Entity):
 
     def get_description(self):
         return self.description
-    
+
     def display_monster(self):
         print(f'Monster Name: {self.get_name()}')
         print(f'Monster Health: {self.get_health()}')
         print(f'Monster Description: {self.get_description()}')
         print(f'Damage: {self.get_damage()}\n')
-        
-        
+
+
 
 # test
 # person1 = Player("Lleyton",100000,10,[1,2])
@@ -132,6 +132,6 @@ class Monsters(Entity):
 #     "LEFT" : None
 # }
 # print(person1.move("RIGHT",adj_tiles))
-    
-    
-    
+
+
+
