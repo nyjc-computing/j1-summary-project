bcb2464a95b19bd42c4c969008037d678778b988
nyjc.computing <164971954+nycomp@users.noreply.github.com>
2026-08-17 08:54:14 +0800

    Refactor game loop to use 'mud' instance
    Refactor game object instantiation and method calls to use 'mud' instead of 'game'.

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-# Import statements
-import mud
+"""main.py
 
+The main game loop.
+"""
+# By convention, import statements go at the top of the file
+
+import game
 import data
 
+
 if __name__ == "__main__":
-    game = mud.Game()
+    mud = game.Game()
     mud.welcome()
     player = data.create_player()
-    game.add_player(player)
-    while not game.is_gameover():
-        choices = game.get_options()
+    mud.add_player(player)
+    while not mud.is_gameover():
+        choices = mud.get_options()
         choice = data.prompt_player_choice(choices)
-        actions = game.get_actions(choice)
-        game.execute(actions)
-        data.display(game.status())
-    mud.epilogue()
+        actions = mud.get_actions(choice)
+        mud.execute(actions)
+        data.display(mud.status())
+    game.epilogue()
+    
